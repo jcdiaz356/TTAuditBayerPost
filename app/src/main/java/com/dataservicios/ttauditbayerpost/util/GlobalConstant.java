9f231cd9d4d483103ae6697b08e9834a0deba69c
Jaime <jcdiaz356@hotmasil.com>
2017-09-28 12:54:00 -0500

    Campana 96

--- app/src/main/java/com/dataservicios/ttauditbayerpost/util/GlobalConstant.java
+++ app/src/main/java/com/dataservicios/ttauditbayerpost/util/GlobalConstant.java
@@ -14,21 +14,21 @@ public final class GlobalConstant {
     public static String directory_images = "/Pictures/" ;
     public static String type_aplication = "android";
     public static int[] poll_id = new int[]{
-          1839, // 1618, //0	  // 1553, 	¿Se encuentra abierto el establecimiento?
-          1840, // 1619, //1	  // 1554, 	¿Tiene exhibición Bayer?
-          1841, // 1620, //2	  // 1555, 	¿Se recomendo el Producto?
-          1842, // 1621, //3	  // 1556, 	¿Qué Producto recomendo?
-          1843, // 1622, //4	  // 1557, 	¿Tiene Stock?
-          1844, // 1623, //5	  // 1558, 	¿Recibio Premio?
-          1845, // 1624, //6	  // 1598, 	¿Tienes Naproxeno sódico genérico de 550mg?
-          1846, // 1625, //7	  // 1599, 	¿Cuantas pastillas compra un consumidor por vez de Naproxeno sódico 550mg genérico?marca?
-          1847, // 1626, //8	  // 1600, 	¿Cuantas pastillas viene en un blíster?
-          1848, // 1627, //9	  // 1601, 	¿Cuál es el precio de la pastilla?
-          1849, // 1628, //10     // 1602, 	¿Cuál es el precio del blíster?
-          1850, // 1629, //11     // 1603, 	Ahora que no hay Dolocordralan en el mercado que producto similar recomienda?
-          1851, // 1630, //12     // 1604, 	Doloneurobion vienen realizando algún tipo de actividad promocional con ustedes?
-          1852, // 1633, //13               Qué tipo de actividades viene realizando Doloneurobion con ustedes?
-          1853, // 1634, //14               Cada cuánto tiempo lo visita un representante/promotor de Doloneurobion?
+           1618, //0	  // 1553, 	¿Se encuentra abierto el establecimiento?
+           1619, //1	  // 1554, 	¿Tiene exhibición Bayer?
+           1620, //2	  // 1555, 	¿Se recomendo el Producto?
+           1621, //3	  // 1556, 	¿Qué Producto recomendo?
+           1622, //4	  // 1557, 	¿Tiene Stock?
+           1623, //5	  // 1558, 	¿Recibio Premio?
+           1624, //6	  // 1598, 	¿Tienes Naproxeno sódico genérico de 550mg?
+           1625, //7	  // 1599, 	¿Cuantas pastillas compra un consumidor por vez de Naproxeno sódico 550mg genérico?marca?
+           1626, //8	  // 1600, 	¿Cuantas pastillas viene en un blíster?
+           1627, //9	  // 1601, 	¿Cuál es el precio de la pastilla?
+           1628, //10     // 1602, 	¿Cuál es el precio del blíster?
+           1629, //11     // 1603, 	Ahora que no hay Dolocordralan en el mercado que producto similar recomienda?
+           1630, //12     // 1604, 	Doloneurobion vienen realizando algún tipo de actividad promocional con ustedes?
+           1633, //13               Qué tipo de actividades viene realizando Doloneurobion con ustedes?
+           1634, //14               Cada cuánto tiempo lo visita un representante/promotor de Doloneurobion?
     } ;
 
     public static int[] audit_id = new int[]{
